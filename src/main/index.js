f840d941e272c79e3167e6f6995b9d3da68cb795
Ravikumar Pawar <ravi.ravipawar17@gmail.com>
2024-12-29 15:26:34 +0530

    imporved css

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,4 @@
-// src/main/index.js
-import { app, BrowserWindow, ipcMain } from 'electron'; // Import ipcMain here
-
+import { app, BrowserWindow, ipcMain } from 'electron';
 import os from 'os';
 import { join } from 'path';
 import { setupFileOperations } from './lib/fileops.js';
@@ -9,7 +7,6 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 900,
     height: 670,
-    
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       contextIsolation: true,
@@ -17,11 +14,18 @@ function createWindow() {
     },
   });
 
+  // Maximize the window to full screen but still have window controls
+  mainWindow.maximize(); // This will maximize the window
+  // Alternatively, if you want a true fullscreen window, use:
+  // mainWindow.setFullScreen(true); // This will make it a full-screen window
+
   if (process.env.ELECTRON_RENDERER_URL) {
     mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL);
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'));
   }
+
+  // You can also listen for resizing and other window-related events if needed
 }
 
 app.whenReady().then(() => {
