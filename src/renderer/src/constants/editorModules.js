d11ed9d7840e58bac878ac71fdc033a2b8ecd904
Ravikumar Pawar <ravi.ravipawar17@gmail.com>
2024-12-29 21:35:20 +0530

    open file separted

--- src/renderer/src/constants/editorModules.js
+++ src/renderer/src/constants/editorModules.js
@@ -1,5 +1,5 @@
+import { openFile } from '../services/fileOperations.js';
 import { PAGE_SIZES } from "./pageSizes";
-
 
 // Undo and redo functions for Custom Toolbar
 function undoChange() {
@@ -13,24 +13,7 @@ function redoChange() {
 
 // Renderer process: src/preload/index.js
 
-export const openFile = async () => {
-    try {
-        // Call the Electron API to open a file
-        const result = await window.electronAPI.openFile();
 
-        // Check if an error is returned from the Electron API
-        if (result && result.error) {
-            throw new Error(result.error);
-        }
-
-        // Assuming 'result' contains the file content, load it into the Quill editor
-        const quill = document.querySelector('.ql-editor'); // Get the Quill editor instance
-        quill.innerHTML = result.content; // Set the file content to the editor
-    } catch (error) {
-        console.error('Error opening file:', error);
-        alert('Failed to open file');
-    }
-};
 
 
 const saveFile = () => {
